e46569fff7e3939430623b921a613bad04843211
ynikalnui <ovskakun@gmail.com>
2024-10-18 00:55:10 +0100

    messages file removed

--- src/handlers/messages.py
+++
@@ -1,7 +0,0 @@
-from aiogram import types, Router
-
-message_router = Router(name=__name__)
-
-@message_router.message()
-async def echo_message(message: types.Message):
-    await message.answer(message.text)
