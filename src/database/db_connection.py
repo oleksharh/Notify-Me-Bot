82e15c1a6cfdc7816d3c2d5ff9c9475fe78cdef8
Oleh Skakun HIC <olehskakunhic@MacBook-Pro-24.local>
2024-10-17 17:18:51 +0100

    db connected

---
+++ src/database/db_connection.py
@@ -0,0 +1,15 @@
+import certifi
+import motor.motor_asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env.local')
+
+DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
+
+if DB_CONNECTION_STRING is None:
+    raise ValueError("No MongoDB connection string found in environment variables")
+
+client = motor.motor_asyncio.AsyncIOMotorClient(DB_CONNECTION_STRING, tlsCAFile=certifi.where())
+
+db = client["notify-me-db"]
