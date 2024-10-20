c077944760ea605003ee9de21c0b067395afe887
oleksharh <oleksharh@gmail.com>
2024-10-20 20:24:24 +0100

    config.py has been added

--- src/config.py
+++ src/config.py
@@ -6,3 +6,7 @@ load_dotenv(".env.local")
 API_TOKEN = os.getenv("API_TOKEN")
 if not API_TOKEN:
     raise ValueError("No API token found. Please add your bot token to .env.local.")
+
+DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
+if not DB_CONNECTION_STRING:
+    raise ValueError("DB connection wasn't set, please check the status of your DB String")
