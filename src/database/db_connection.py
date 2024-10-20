c6c13e8d1397de12a174d896b55ba1deafc10eec
oleksharh <oleksharh@gmail.com>
2024-10-20 20:24:24 +0100

    config.py has been added

--- src/database/db_connection.py
+++ src/database/db_connection.py
@@ -1,15 +1,9 @@
 import certifi
 import motor.motor_asyncio
-from dotenv import load_dotenv
-import os
-
-load_dotenv(".env.local")
-
-DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
-
-if DB_CONNECTION_STRING is None:
-    raise ValueError("No MongoDB connection string found in environment variables")
+from ..config import DB_CONNECTION_STRING
 
 client = motor.motor_asyncio.AsyncIOMotorClient(DB_CONNECTION_STRING, tlsCAFile=certifi.where())
 
 db = client["notify-me-db"]
+
+__all__ = ["db"]
