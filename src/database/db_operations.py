7e5b48523199549e061f1ca17926bbd81098223b
oleksharh <oleksharh@gmail.com>
2024-10-22 21:55:56 +0100

    Added further manage functions

--- src/database/db_operations.py
+++ src/database/db_operations.py
@@ -2,25 +2,23 @@ from .db_connection import db
 from datetime import datetime
 from bson import ObjectId
 
+reminder_collection = db["reminders"]
 
 async def get_reminders(user_id: int, chat_id: int):
-    reminder_collection = db["reminders"]
     return await reminder_collection.find({"user_id": user_id, "chat_id": chat_id}).to_list(length=None)
 
 
 async def get_reminder_by_id(task_id):
-    reminder_collection = db["reminders"]
     return await reminder_collection.find_one({"_id": ObjectId(task_id)})
 
 
 async def add_reminder(user_id: int, chat_id: int, message: str, priority: int) -> str:
-    reminder_collection = db["reminders"]
-
     new_record = {
         "user_id": user_id,
         "chat_id": chat_id,
         "message": message,
         "priority": priority,
+        "status": False,
         "timestamp": datetime.now()
     }
 
@@ -28,3 +26,20 @@ async def add_reminder(user_id: int, chat_id: int, message: str, priority: int) 
     result = await reminder_collection.insert_one(new_record)
 
     return str(result.inserted_id)
+
+
+async def remove_reminder(task_id: str):
+    await reminder_collection.delete_one({"_id": ObjectId(task_id)})
+    return True
+
+
+async def update_task_status(task_id: str | ObjectId, status: bool):
+    result = await reminder_collection.update_one(
+        {"_id": ObjectId(task_id)},
+        {"$set": {"status": status}}
+    )
+
+    if result.matched_count == 1:
+        return True  # Successfully updated
+    else:
+        return False  # Task not found or update failed
--- src/utils/keyboards.py
+++ src/utils/keyboards.py
@@ -1,4 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from bson import ObjectId
+
 
 def create_priority_menu(user_id, chat_id, message):
     buttons = [
@@ -8,19 +10,46 @@ def create_priority_menu(user_id, chat_id, message):
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
-def create_edit_menu(reminders):
+
+def create_manage_menu(reminders):
     buttons = [
-        [InlineKeyboardButton(text="Exit", callback_data="edit_exit")],
+        [InlineKeyboardButton(text="🚪 Exit", callback_data="manage_exit")],
         *[
-            [InlineKeyboardButton(text=f"Edit Task {reminder['message']}", callback_data=f"edit_{reminder['_id']}")]
+            [InlineKeyboardButton(text=f"Edit Task {reminder['message']}", callback_data=f"manage_{reminder['_id']}")]
             for reminder in reminders
         ]
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
-def create_manage_menu():
+
+def create_list_menu():
     buttons = [
-        [InlineKeyboardButton(text=f"Back", callback_data=f"manage_back")],
-        [InlineKeyboardButton(text=f"Manage", callback_data=f"manage_manage")]
+        [InlineKeyboardButton(text=f"Back", callback_data=f"list_back")],
+        [InlineKeyboardButton(text=f"Manage", callback_data=f"list_manage")]
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+
+def create_edit_menu(task_id: str | ObjectId):
+    buttons = [
+        [InlineKeyboardButton(text=f"Manage Priority", callback_data=f"edit_priority_{task_id}")],
+        [InlineKeyboardButton(text=f"Set task as finished", callback_data=f"edit_status_{task_id}")],
+    ]
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+
+def create_priority_manage_menu(task_id: str | ObjectId):
+    buttons = [
+        [InlineKeyboardButton(text=f"Low", callback_data=f"priority_1_{task_id}")],
+        [InlineKeyboardButton(text=f"Medium", callback_data=f"priority_2_{task_id}")],
+        [InlineKeyboardButton(text=f"High", callback_data=f"priority_3_{task_id}")]
+    ]
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+def create_status_manage_menu(task_id: str | ObjectId):
+    buttons = [
+        [InlineKeyboardButton(text=f"Not Finished", callback_data=f"status_false_{task_id}")],
+        [InlineKeyboardButton(text=f"Finished", callback_data=f"status_true_{task_id}")],
+    ]
