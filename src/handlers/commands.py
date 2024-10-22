9b1b95e809e8ebc5a9a8961576fb01348d4a9478
oleksharh <oleksharh@gmail.com>
2024-10-22 21:55:56 +0100

    Added further manage functions

--- src/handlers/commands.py
+++ src/handlers/commands.py
@@ -1,7 +1,11 @@
 from aiogram import types, Router, F
 from aiogram.filters import Command
-from src.database.db_operations import add_reminder, get_reminders, get_reminder_by_id
-from src.utils.keyboards import create_edit_menu, create_priority_menu, create_manage_menu
+from bson import ObjectId
+
+from src.database.db_operations import add_reminder, get_reminders, get_reminder_by_id, remove_reminder, \
+    update_task_status
+from src.utils.keyboards import create_manage_menu, create_priority_menu, create_list_menu, create_edit_menu, \
+    create_priority_manage_menu, create_status_manage_menu
 
 command_router = Router(name="command_router")
 
@@ -30,7 +34,7 @@ async def send_list(message: types.Message) -> None:
         return
 
     tasks_str = format_task_list(reminders)
-    keyboard = create_manage_menu()
+    keyboard = create_list_menu()
 
     await message.answer(text="Your tasks are: \n\n" + tasks_str, reply_markup=keyboard)
 
@@ -51,8 +55,8 @@ def format_task_list(reminders: list) -> str:
 
 
 # Manage Tasks Callback Query Handler
-@command_router.callback_query(lambda c: c.data.startswith("manage_"))
-async def manage(callback_query: types.CallbackQuery):
+@command_router.callback_query(lambda c: c.data.startswith("list_"))
+async def list_manage(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
     chat_id = callback_query.message.chat.id
     function = callback_query.data.split("_")[-1]
@@ -82,31 +86,63 @@ async def manage_menu(message: types.Message, user_id=0, chat_id=0):
         await message.answer("No tasks available.")
         return
 
-    keyboard = create_edit_menu(reminders)
+    keyboard = create_manage_menu(reminders)
     await message.answer("Here are your tasks. Choose one to edit:", reply_markup=keyboard)
 
 
-@command_router.callback_query(lambda c: c.data.startswith("edit_"))
+@command_router.callback_query(lambda c: c.data.startswith("manage_"))
 async def edit_task(callback_query: types.CallbackQuery):
     task_id = callback_query.data.split("_")[1]
 
+    await  callback_query.message.delete()
+
     if task_id == "exit":
-        await  callback_query.message.delete()
         await callback_query.message.answer("You are back in the main menu!\n"
                                             "Please enter your next task.")
         return
 
     task = await get_reminder_by_id(task_id)
 
-    await callback_query.message.delete()
-
     if task:
-        await callback_query.message.answer(f"You selected: {task["message"]}.")
-        await callback_query.message.answer("Please enter the new task details.")
+        await callback_query.message.answer(f"You selected: {task["message"]}. {type(task["_id"])}")
+        await edit_menu(callback_query.message, task["_id"])
     else:
         await callback_query.message.answer("Task not found.")
 
     await callback_query.answer()
+
+
+async def edit_menu(message: types.Message, task_id: str | ObjectId):
+    keyboard = create_edit_menu(task_id)
+    await message.answer("Choose what you want to perform", reply_markup=keyboard)
+
+
+def change_status(message: types.Message, task_id: str | ObjectId, status: bool):
+
+    update_task_status(task_id, status)
+
+
+def change_priority(message: types.Message, task_id: str | ObjectId, priority: int):
+    pass
+
+
+@command_router.callback_query(lambda c: c.data.startswith("edit_"))
+async def edit_task(callback_query: types.CallbackQuery):
+    function = callback_query.data.split("_")[1]
+    task_id = callback_query.data.split("_")[2]
+
+    await callback_query.message.delete()
+
+    if function == "priority":
+        keyboard = create_priority_manage_menu(task_id)
+        await callback_query.message.edit_reply_markup(text="Choose wanted priority",reply_markup=None)
+        change_priority(callback_query.message, task_id, 0)
+    elif function == "status":
+        keyboard = create_status_manage_menu(task_id)
+        await callback_query.message.edit_reply_markup(reply_markup=None)
+        # change_status(task_id,)
+    else:
+        await callback_query.message.answer("Mistake has occurred, check callback_data\n")
 
 
 # Handle User Text Input
