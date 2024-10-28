d610c8c3467cf26c827e43f941007da1170f0f53
ynikalnui <ovskakun@gmail.com>
2024-10-28 00:51:38 +0000

    sheduled tasks added

--- src/utils/scheduled_tasks.py
+++ src/utils/scheduled_tasks.py
@@ -9,7 +9,35 @@ class ScheduledTasks:
         self.bot = bot
         self.setup_tasks()
 
-    @staticmethod
-    def setup_tasks():
-        # @aiocron.crontab('* * * * * */10')
-        pass
+    def setup_tasks(self):
+        # Send all priority levels at 10:00
+        @aiocron.crontab('0 10 * * *')
+        async def task_10():
+            await self.send_reminders([0, 1, 2])
+
+        # Send priority 1 and 2 at 15:00
+        @aiocron.crontab('0 15 * * *')
+        async def task_15():
+            await self.send_reminders([1, 2])
+
+        # Send priority 2 only at 20:00
+        @aiocron.crontab('0 20 * * *')
+        async def task_20():
+            await self.send_reminders([2])
+
+    async def send_reminders(self, priorities):
+        reminders = await self.db.get_all_reminders()
+
+        filtered_reminders = [
+            reminder for reminder in reminders
+            if reminder['status'] is False and reminder['priority'] in priorities
+        ]
+
+        for reminder in filtered_reminders:
+            try:
+                await self.bot.send_message(
+                    chat_id=reminder['chat_id'],
+                    text=f"Reminder: {reminder['message']}"
+                )
+            except Exception as e:
+                print(f"Failed to send reminder to chat_id {reminder['chat_id']}: {e}")
